01d73b6eff01a8b8c86f1e6d127e939e2806a34d
Nhật Nguyễn Đình <nhatnguyendinh@Nhats-MacBook-Air.local>
2023-03-24 21:32:28 +0700

    update new packet

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
-app.get('/tin-tuc', (reg, res) => res.send('Hello World!'));
+
+app.use(morgan('combined'))
+
+app.get('/', (reg, res) => {
+   return res.send(`
+        <h1 style="color: red;">Hello World!</h1>
+   `);
+})
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
