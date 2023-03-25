aadd59df5c92b918d8e4aab6cab2dc64d6bc56e8
Nhật Nguyễn Đình <nhatnguyendinh@Nhats-MacBook-Air.local>
2023-03-25 07:16:30 +0700

    using template engine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+const path = require('path')
 const express = require('express');
-const handlebars = require('express-handlebars');
+const { engine } = require ('express-handlebars');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
@@ -7,10 +8,18 @@ const port = 3000;
 app.use(morgan('combined')) //http logger
 
 //template engine
-app.engine('handlebars',handlebars());
-app.set('view engine', 'handlebars');
+
+app.engine('hbs', engine({
+   extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
 
 app.get('/', (reg, res) => {
    res.render('home');
+})
+
+app.get('/news', (reg, res) => {
+   res.render('news');
 })
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
